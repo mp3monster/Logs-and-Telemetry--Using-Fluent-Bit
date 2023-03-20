c02a3cd150bac1c0dd6e803df0701f7efa055d6a
Phil Wilkins <phil@mp3monster.org>
2023-03-20 19:04:06 +0000

    additioanl use cases and configs

---
+++ chapter1/hello-world-3.cli.bat
@@ -0,0 +1,1 @@
+fluent-bit -q -i dummy -t dummy1 -p dummy="{\"hello\":\"my world\"}"  -i dummy -t dummy2 -p dummy="{\"more\":\"stuff\"}" -o stdout -m "*" -o stdout -m "*" -p Format=json -p json_date_format=iso8601
