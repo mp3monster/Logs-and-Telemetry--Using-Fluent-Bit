277632e148c5c9e09fa6ceef80bc73af4e5d5050
Phil Wilkins <phil@mp3monster.org>
2023-08-31 18:04:35 +0100

    configuration for chapter 4 examples

---
+++ chapter4/start-wiremock.bat
@@ -0,0 +1,5 @@
+REM docker run -it --rm -p 8080:8080 -p 8443:8443 --name wiremock --volume .\wiremock\mappings.json:/home/wiremock/mappings/mappings.json wiremock/wiremock:2.35.0  --verbose
+docker run -it --rm -p 8080:8080 -p 8443:8443 --name wiremock --volume .\wiremock:/home/wiremock/mappings wiremock/wiremock:2.35.0  --verbose
+REM # https://wiremock.org/docs/standalone/docker/
+REM # http://localhost:8080/__admin/mappings
+REM # http://localhost:8080/__admin/requests
