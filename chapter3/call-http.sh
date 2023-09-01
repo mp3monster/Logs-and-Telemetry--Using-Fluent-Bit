2ff9821ecea2b27925d776bf2d43c5bb1be15ec2
Phil Wilkins <phil@mp3monster.org>
2023-09-01 23:32:42 +0100

    can moved files and add typo fixes

--- chapter3/call-http.sh
+++
@@ -1,7 +0,0 @@
-curl --location 'localhost:9881' \
---header 'Content-Type: application/json' \
---data '{
-    "hello": "Fluent"
-}' \
---output results.txt \
---include
--- chapter3/call-http2.sh
+++
@@ -1,7 +0,0 @@
-curl --location 'localhost:9891' \
---header 'Content-Type: application/json' \
---data '{
-    "hello": "to another Fluent endpoint"
-}' \
---include \
---output response.txt
