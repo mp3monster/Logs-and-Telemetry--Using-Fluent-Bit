aba1f41ee14c9835c7c49a9aec3f71ae3d38faa9
Phil Wilkins <phil@mp3monster.org>
2023-10-06 20:13:13 +0100

    Simple Lua script

---
+++ chapter6/fluentbit/attribute-count.lua
@@ -0,0 +1,29 @@
+local function elementCounter (record)
+  -- this function can be used recursively so we can count nested elements
+  counter = 0
+  for key,value in pairs(record) do
+    if (type(value) == "table") then
+      counter = counter + elementCounter(value)
+    else
+      counter=counter+1
+      print(string.format("-->[%d] %s --> %s",counter, key, value))
+    end
+  end
+  return counter
+end
+
+function cb_addElementCount(tag, timestamp, record)
+  -- we need to indicate back to Fluent Bit that the record will have changed, but not the timestamp
+  local code = 2;
+
+  if (table['remote_user'] ~= nil) then
+    -- if the remote_user attribute exists let's change it to be Lua
+    record ['remote_user'] = "Lua"
+  end
+
+  -- add a new element with the count of elements in the structure passed
+  record["element_count"] = (elementCounter(record) + 1)
+
+  return code, timestamp, record
+
+end
