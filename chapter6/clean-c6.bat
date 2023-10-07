7d4f89ea5a114152c274a5d85c1242f1ea463475
Phil Wilkins <phil@mp3monster.org>
2023-10-07 19:27:02 +0100

    Extend the clean up to cover an additional file

--- chapter6/clean-c6.bat
+++ chapter6/clean-c6.bat
@@ -2,3 +2,4 @@ del apache-log
 del nginx.json.log
 del nginx-log
 del codedJSON.log
+del Apps-all.txt
--- chapter6/clean-c6.sh
+++ chapter6/clean-c6.sh
@@ -2,3 +2,4 @@ rm apache-log
 rm nginx.json.log
 rm nginx-log
 rm codedJSON.log
+rm Apps-all.txt
