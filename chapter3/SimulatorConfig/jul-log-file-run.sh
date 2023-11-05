4845cff3567143ebfe5b41cf5d470382427db532
Phil Wilkins <phil@mp3monster.org>
2023-11-05 17:04:13 +0000

    configure for containerized execution

---
+++ chapter3/SimulatorConfig/jul-log-file-run.sh
@@ -0,0 +1,7 @@
+# $flbBookRootDir - see Appendix A for configuration
+docker run -v .:/vol/log \
+  -v %flbBookRootDir%/chapter3/SimulatorConfig/:/vol/conf \
+  -v %flbBookRootDir%/TestData/:/vol/test-data \
+  --env run_props=jul-log-file.properties \
+  --env data=medium-source.txt \
+  logsimcontainer-logger
