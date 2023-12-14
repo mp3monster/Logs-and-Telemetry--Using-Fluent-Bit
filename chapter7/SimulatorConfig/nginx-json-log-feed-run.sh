205eff9a6cd809613032b1720350dd4d04100eb6
Phil Wilkins <phil@mp3monster.org>
2023-12-14 21:26:19 +0000

    Correct chapter number and add script to run as containers

---
+++ chapter7/SimulatorConfig/nginx-json-log-feed-run.sh
@@ -0,0 +1,7 @@
+# $flbBookRootDir - see Appendix A for configuration
+docker run -v .:/vol/log \
+     -v $flbBookRootDir/chapter7/SimulatorConfig/:/vol/conf \
+     -v $flbBookRootDir/TestData/:/vol/test-data \
+     --env run_props=nginx-malformed-json-log-feed.properties \
+     --env data=nginx.malformed.json.log.txt \
+     logsimcontainer-logger 
