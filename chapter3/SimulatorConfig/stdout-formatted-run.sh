baf94f1fc435a02dea287f385b4ea3901530be1f
Phil Wilkins <phil@mp3monster.org>
2024-05-16 00:38:00 +0100

    changed command from multi-line to single line stmt as multiline was upsetting Linux

--- chapter3/SimulatorConfig/stdout-formatted-run.sh
+++ chapter3/SimulatorConfig/stdout-formatted-run.sh
@@ -1,6 +1,2 @@
 # $flbBookRootDir - see Appendix A for configuration
-docker run -v .:/vol/log -v $flbBookRootDir/chapter3/SimulatorConfig/:/vol/conf \
-           -v $flbBookRootDir/TestData/:/vol/test-data \
-           --env run_props=stdout-formatted.properties \
-           --env data=medium-source.txt \
-           logsimcontainer-logger 
+docker run -v .:/vol/log -v $flbBookRootDir/chapter3/SimulatorConfig/:/vol/conf -v $flbBookRootDir/TestData/:/vol/test-data --env run_props=stdout-formatted.properties --env data=medium-source.txt logsimcontainer-logger 
