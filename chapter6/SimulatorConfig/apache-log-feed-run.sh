2db8dd61e1bc218f7ab019bdda5a5f510b4d9196
Phil Wilkins <phil@mp3monster.org>
2024-05-16 01:00:29 +0100

    changed from multiline command to single line

--- chapter6/SimulatorConfig/apache-log-feed-run.sh
+++ chapter6/SimulatorConfig/apache-log-feed-run.sh
@@ -1,7 +1,2 @@
 # $flbBookRootDir - see Appendix A for configuration
-docker run -v .:/vol/log \
-     -v $flbBookRootDir/chapter6/SimulatorConfig/:/vol/conf \
-     -v $flbBookRootDir/TestData/:/vol/test-data \
-     --env run_props=apache-log-feed.properties \
-     --env data=Apache.log.txt \
-     logsimcontainer-logger 
+docker run -v .:/vol/log -v $flbBookRootDir/chapter6/SimulatorConfig/:/vol/conf -v $flbBookRootDir/TestData/:/vol/test-data --env run_props=apache-log-feed.properties --env data=Apache.log.txt logsimcontainer-logger 
--- chapter6/SimulatorConfig/encoded-logs-run.sh
+++ chapter6/SimulatorConfig/encoded-logs-run.sh
@@ -1,7 +1,2 @@
 # $flbBookRootDir - see Appendix A for configuration
-docker run -v .:/vol/log \
-     -v $flbBookRootDir/chapter6/SimulatorConfig/:/vol/conf \
-     -v $flbBookRootDir/TestData/:/vol/test-data \
-     --env run_props=encoded-logs.properties \
-     --env data=coded-json.txt \
-     logsimcontainer-logger 
+docker run -v .:/vol/log -v $flbBookRootDir/chapter6/SimulatorConfig/:/vol/conf -v $flbBookRootDir/TestData/:/vol/test-data --env run_props=encoded-logs.properties --env data=coded-json.txt logsimcontainer-logger 
