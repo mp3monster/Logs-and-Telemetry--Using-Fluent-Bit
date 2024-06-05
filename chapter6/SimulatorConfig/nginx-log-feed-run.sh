054e5a53a2c56c5b2398d5ecae6b14b79ae2629d
Phil Wilkins <phil@mp3monster.org>
2024-06-05 21:03:24 +0100

    correct filename references for case / missing character

--- chapter6/SimulatorConfig/nginx-log-feed-run.sh
+++ chapter6/SimulatorConfig/nginx-log-feed-run.sh
@@ -1,2 +1,2 @@
 # $flbBookRootDir - see Appendix A for configuration
-docker run -v .:/vol/log -v $flbBookRootDir/chapter6/SimulatorConfig/:/vol/conf -v $flbBookRootDir/TestData/:/vol/test-data --env run_props=nginx-log-feed.properties --env data=Nginx.log.txt  logsimcontainer-logger 
+docker run -v .:/vol/log -v $flbBookRootDir/chapter6/SimulatorConfig/:/vol/conf -v $flbBookRootDir/TestData/:/vol/test-data --env run_props=nginx-log-feed.properties --env data=nginx.log.txt  logsimcontainer-logger 
--- chapter6/SimulatorConfig/nginx-malformed-json-log-feed-run.bat
+++ chapter6/SimulatorConfig/nginx-malformed-json-log-feed-run.bat
@@ -1,2 +1,2 @@
 REM # $flbBookRootDir - see Appendix A for configuration
-docker run -v .:/vol/log -v %flbBookRootDir%/chapter6/SimulatorConfig/:/vol/conf -v %flbBookRootDir%/TestData/:/vol/test-data --env run_props=nginx-malformed-json-log-feed.properties --env data=ginx.malformed.json.log.txt logsimcontainer-logger 
+docker run -v .:/vol/log -v %flbBookRootDir%/chapter6/SimulatorConfig/:/vol/conf -v %flbBookRootDir%/TestData/:/vol/test-data --env run_props=nginx-malformed-json-log-feed.properties --env data=nginx.malformed.json.log.txt logsimcontainer-logger 
