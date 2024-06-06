93c41c376ca4a610c0f4198c07cddc06d193e0e5
Phil Wilkins <phil@mp3monster.org>
2024-06-06 21:11:43 +0100

    correct formatting

--- chapter5/start-wiremock.sh
+++ chapter5/start-wiremock.sh
@@ -1,4 +1,4 @@
-docker run -it --rm -p 8080:8080 -p 8443:8443 --name wiremock --volume .\wiremock:/home/wiremock/mappings wiremock/wiremock:2.35.0  --verbose
+docker run -it --rm -p 8080:8080 -p 8443:8443 --name wiremock --volume ./wiremock:/home/wiremock/mappings wiremock/wiremock:2.35.0 --verbose
 # https://wiremock.org/docs/standalone/docker/
 # http://localhost:8080/__admin/mappings
 # http://localhost:8080/__admin/requests
